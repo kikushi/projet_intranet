5ca8a1606009e8db1a7541d84a4306fddbb5e525
keith <keitadollard0@gmail.com>
2019-06-27 08:22:41 -0400

    Generation du code barre

--- app/src/main/java/com/example/projet_intranet/GeneratedCode.java
+++ app/src/main/java/com/example/projet_intranet/GeneratedCode.java
@@ -1,13 +1,64 @@
 package com.example.projet_intranet;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class GeneratedCode extends AppCompatActivity {
+
+    ImageView image;
+    Button btnOk;
+    String code;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generated_code);
+
+        image = (ImageView) findViewById(R.id.image);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        textView = (TextView) findViewById(R.id.msg);
+
+        //code a remplacer par id du colis
+        code = textView.getText().toString().trim();
+
+        btnOk.setOnClickListener(decoBtnOK);
+
+        // generation du code qr
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(code, BarcodeFormat.QR_CODE, 200, 200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            image.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private View.OnClickListener decoBtnOK = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            decoBtnOkClicked();
+        }
+    };
+
+    private void decoBtnOkClicked() {
+
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
     }
 }
+
+
